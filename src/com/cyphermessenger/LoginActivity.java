ef2496a47a8b82e97e21c984ca88402692cdf210
Kairos90 <freip@hotmail.it>
2014-03-06 18:02:27 +0100

    Recreated LoginActivity

---
+++ src/com/cyphermessenger/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.cyphermessenger;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class LoginActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		return super.onCreateOptionsMenu(menu);
+	}
+}
