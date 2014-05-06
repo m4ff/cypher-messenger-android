c74a7075ff083c0f0776c9aa12fc8513172f7eac
Kairos90 <freip@hotmail.it>
2014-05-06 18:53:29 +0200

    Changes

--- CypherMessenger/Cypher Messenger/src/main/java/com/cyphermessenger/AddContactActivity.java
+++ CypherMessenger/Cypher Messenger/src/main/java/com/cyphermessenger/AddContactActivity.java
@@ -1,11 +1,14 @@
 package com.cyphermessenger;
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 public class AddContactActivity extends ActionBarActivity {
@@ -15,19 +18,14 @@ public class AddContactActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
 
-        EditText contactName = (EditText) findViewById(R.id.add_contact_name);
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.add_contact, menu);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchContact = (SearchView) menu.getItem(R.id.add_contact_search_view).getActionView();
+        searchContact.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        return super.onOptionsItemSelected(item);
     }
 }
